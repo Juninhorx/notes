b0156b53f5fab0ab92573be3ef9683bcd464a522
Ricardo Xavier <rxc.projects@gmail.com>
2024-12-05 11:36:37 -0300

    login route, validation

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,10 @@
 <?php
 
-use App\Http\Controllers\MainController;
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
+
+//Auth routes     url --------------controller------,---metodo do controller
+Route::get('/login', [AuthController::class, 'login']);
+Route::post('/loginSubmit', [AuthController::class, 'loginSubmit']);
+Route::get('/logout', [AuthController::class, 'logout']);
