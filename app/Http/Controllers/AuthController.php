d26567c4435e99a8aa0390d011bfbd22a63c377c
Ricardo Xavier <rxc.projects@gmail.com>
2024-12-07 09:24:28 -0300

    login validation

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -36,17 +36,41 @@ class AuthController extends Controller
         $username = $request->input('text_username');
         $password = $request->input('text_password');
 
-        // get all the users from database
+        // check if user exists
+        $user = User::where('username', $username)
+                ->where('deleted_at', NULL)
+                ->first();
 
-        // diretamente
-        // $users = User::all()->toArray();
+        if (!$user){
+            return redirect()
+                    ->back()
+                    ->withInput()
+                    ->with('loginError', 'Usuário ou senha incorretos.');
+        }
 
-        // como instância da classe User
-        $UserModel = new User();
-        $users = $UserModel->all()->toArray();
+        // check if password is correct
 
-        echo '<pre>';
-        print_r($users);
+        if (!password_verify($password, $user->password)) {
+            return redirect()
+                    ->back()
+                    ->withInput()
+                    ->with('loginError', 'Usuário ou senha incorretos.');
+        }
+
+        // Update last login
+
+        $user->last_login = date('Y-m-d H:i:s');
+        $user->save();
+
+        // login user
+        session([
+            'user' => [
+                'id' => $user->id,
+                'username' => $user->username
+            ]
+        ]);
+
+        echo 'LOGIN CONCLUÍDO COM SUCESSO!';
     }
 
     public function logout()
