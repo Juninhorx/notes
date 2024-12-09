a798cb4cdfc907ea5c16314eeed7a4fcbd723092
Ricardo Xavier <rxc.projects@gmail.com>
2024-12-09 08:23:29 -0300

    delete and edit notes decrypt id

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use Illuminate\Contracts\Encryption\DecryptException;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Crypt;
 
 class MainController extends Controller
 {
@@ -25,4 +27,32 @@ class MainController extends Controller
     {
         echo 'im creating a new note!';
     }
+
+    public function editNote($id)
+    {
+        try {
+            //code...
+            $id = Crypt::decrypt($id);
+            echo $id;
+        } catch (DecryptException $e) {
+
+            return redirect()->route('home');
+        }
+        echo "im editing note with id = $id";
+    }
+
+    public function deleteNote($id)
+    {
+        try {
+            //code...
+            $id = Crypt::decrypt($id);
+            echo $id;
+        } catch (DecryptException $e) {
+
+            return redirect()->route('home');
+        }
+        echo "im deleting note with id = $id";
+    }
+
+
 }
