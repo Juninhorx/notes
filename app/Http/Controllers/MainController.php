b8ba0c0288240806802c967ec38c13b5f84b21dc
Ricardo Xavier <rxc.projects@gmail.com>
2024-12-09 22:40:37 -0300

    edit notes in database

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -67,10 +67,51 @@ class MainController extends Controller
 
     public function editNote($id)
     {
-
         //$id = $this->decryptId($id);
         $id = Operations::decryptId($id);
-        echo "im editing note with id = $id";
+
+        // load note
+        $note = Note::find($id);
+
+        //show edit note view
+        return view('edit_note', ['note' => $note]);
+    }
+
+    public function editNoteSubmit(Request $request){
+
+        // validate request
+        $request->validate(
+            //rules
+            [
+                'text_title' => 'required|min:3|max:200',
+                'text_note' => 'required|min:3|max:3000'
+            ],
+            //messages
+            [
+                'text_title.required' => 'O Título é obrigatório',
+                'text_title.min' => 'O Título deve ter pelo menos :min caracteres',
+                'text_title.max' => 'O Título deve ter no máximo :max caracteres',
+                'text_note.required' => 'A Nota é obrigatória',
+                'text_note.min' => 'A Nota deve ter pelo menos :min caracteres',
+                'text_note.max' => 'A Nota deve ter no máximo :max caracteres'
+            ]
+        );
+
+        // check if note_id exists
+        if ($request->note_id == null) {
+            return redirect()->route('home');
+        }
+        // dcrypt note_id
+        $id = Operations::decryptId($request->note_id);
+        // load note
+        $note = Note::find($id); // Note(model)::find(método stático extendido do Model)
+        // update note
+        $note->title = $request->text_title;
+        $note->text = $request->text_note;
+        $note->save();
+        // redirect to home
+        return redirect()->route('home');
+
     }
 
     public function deleteNote($id)
