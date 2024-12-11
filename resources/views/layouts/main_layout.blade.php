d0d8f921498825f1d940637ae2723b93eb0c4768
Ricardo Xavier <rxc.projects@gmail.com>
2024-12-11 08:58:12 -0300

    fixing blade asset

--- resources/views/layouts/main_layout.blade.php
+++ resources/views/layouts/main_layout.blade.php
@@ -4,15 +4,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>NOTES</title>
-    <link rel="stylesheet" href="{{asset('assets/bootstrap/bootstrap.min.css' )}}">
-    <link rel="stylesheet" href="{{asset('assets/fontawesome/css/all.min.css' )}}">
-    <link rel="shortcut icon" href="{{asset('assets/images/favicon.png' )}}" type="image/png">
+    <link rel="stylesheet" href="{{asset('assets/bootstrap/bootstrap.min.css', true )}}">
+    <link rel="stylesheet" href="{{asset('assets/fontawesome/css/all.min.css', true )}}">
+    <link rel="shortcut icon" href="{{asset('assets/images/favicon.png', true )}}" type="image/png">
 </head>
 <body>
 
     {{-- Vai puxar o elemento como content --}}
     @yield('content')
 
-    <script src="{{asset('assets/bootstrap/bootstrap.bundle.min.js' )}}"></script>
+    <script src="{{asset('assets/bootstrap/bootstrap.bundle.min.js', true)}}"></script>
 </body>
 </html>
